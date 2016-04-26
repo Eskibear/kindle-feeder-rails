c93159357974840a5e33c44291749e6a289bddf9
Eskibear <me@eskibear.info>
2016-04-26 22:59:00 +0800

    modify KindleEar utils

---
+++ lib/kindle/lib/main.py
@@ -0,0 +1,100 @@
+from calibre.ebooks.conversion.mobioutput import MOBIOutput
+import makeoeb
+import base
+import time
+from config import *
+from calibre.utils.bytestringio import byteStringIO
+import __builtin__, logging
+default_log = logging.getLogger()
+__builtin__.__dict__['default_log'] = default_log
+class RailsBook:
+    def __init__(hs):
+        self.title = hs['title']
+        self.masthead_path = hs['masthead_path']
+        self.cover_path = hs['cover_path']
+        self.desc = hs['desc']
+        self.lang = hs['lang']
+        self.feeds = hs['feeds']
+        self.oldest_article = hs['oldest_article']
+
+def GET(bk):
+    opts = makeoeb.getOpts('kindlepw')
+    oeb = makeoeb.CreateOeb(default_log, None, opts)
+
+    title = bk.title
+
+    makeoeb.setMetaData(oeb, title)
+    oeb.container = makeoeb.ServerContainer()
+
+    # cover
+    mhfile = bk.masthead_path
+    coverfile = bk.cover_path
+
+    if mhfile:
+        id_, href = oeb.manifest.generate('masthead', mhfile)  # size:600*60
+        oeb.manifest.add(id_, href, makeoeb.MimeFromFilename(mhfile))
+        oeb.guide.add('masthead', 'Masthead Image', href)
+
+    if coverfile:
+        id_, href = oeb.manifest.generate('cover', coverfile)
+        item = oeb.manifest.add(id_, href, makeoeb.MimeFromFilename(coverfile))
+        oeb.guide.add('cover', 'Cover', href)
+        oeb.metadata.add('cover', id_)
+
+    itemcnt, imgindex = 0, 0
+    from collections import OrderedDict
+    sections = OrderedDict()
+    toc_thumbnails = {}  # map img-url -> manifest-href
+    if not bk:
+        default_log.warn('not exist book <%s>' % bk.title)
+
+
+    book = base.BaseFeedBook(imgindex=imgindex)
+    book.title = bk.title
+    book.description = bk.desc
+    book.language = bk.lang
+    book.oldest_article = bk.oldest_article
+    book.fulltext_by_readability = True
+    feeds = bk.feeds
+    book.feeds = [(feed.title, feed.url, feed.fulltext) for feed in feeds]
+
+    try:
+        for sec_or_media, url, title, content, brief, thumbnail in book.Items(opts):
+            if not sec_or_media or not title or not content:
+                continue
+
+            if sec_or_media.startswith(r'image/'):
+                id_, href = oeb.manifest.generate(id='img', href=title)
+                item = oeb.manifest.add(id_, href, sec_or_media, data=content)
+                if thumbnail:
+                    toc_thumbnails[url] = href
+                imgindex += 1
+            else:
+                # id, href = oeb.manifest.generate(id='feed', href='feed%d.html'%itemcnt)
+                # item = oeb.manifest.add(id, href, 'application/xhtml+xml', data=content)
+                # oeb.spine.add(item, True)
+                sections.setdefault(sec_or_media, [])
+                sections[sec_or_media].append((title, brief, thumbnail, content))
+                itemcnt += 1
+    except Exception as e:
+        default_log.warn("Failure in pushing book '%s' : %s" % (book.title, str(e)))
+    print itemcnt
+    if itemcnt > 0:
+        from utils import InsertToc
+        InsertToc(oeb, sections, toc_thumbnails)
+        oIO = byteStringIO()
+        o = MOBIOutput()
+        o.convert(oeb, oIO, opts, default_log)
+
+        of_name = "/tmp/"+ title + time.strftime("%Y-%m-%d_%H%M", time.localtime()) + ".mobi"
+        with open(of_name, "w") as f:
+            f.write(str(oIO.getvalue()))
+        return of_name
+    return ""
+
+# main
+
+# TODO
+# load json into bk object
+# GET(bk)
+# return output_filename
---
+++ lib/kindle/lib/urlopener.py
@@ -0,0 +1,174 @@
+#!usr/bin/Python
+# -*- coding:utf-8 -*-
+"""为了应付时不时出现的Too many redirects异常，使用此类打开链接。
+此类会自动处理redirect和cookie，同时增加了失败自动重试功能"""
+import urllib, urllib2, Cookie, urlparse, time
+# from google.appengine.api import urlfetch
+# from google.appengine.runtime.apiproxy_errors import OverQuotaError
+from config import CONNECTION_TIMEOUT
+
+class MyResp:
+    def __init__(self, a, b):
+        self.status_code = a
+        self.content = b
+
+
+
+class URLOpener:
+    _codeMapDict = {
+        200 : 'Ok',
+        201 : 'Created',
+        202 : 'Accepted',
+        203 : 'Non-Authoritative Information',
+        204 : 'No Content',
+        205 : 'Reset Content',
+        206 : 'Partial Content',
+        300 : 'Multiple Choices',
+        301 : 'Moved Permanently',
+        302 : 'Found',
+        303 : 'See Other',
+        304 : 'Not Modified',
+        305 : 'Use Proxy',
+        307 : 'Temporary Redirect',
+        400 : 'Bad Request',
+        401 : 'Unauthorized',
+        402 : 'Payment Required',
+        403 : 'Forbidden',
+        404 : 'Not Found',
+        405 : 'Method Not Allowed',
+        406 : 'Not Acceptable',
+        407 : 'Proxy Authentication Required',
+        408 : 'Request Timeout',
+        409 : 'Conflict',
+        410 : 'Gone',
+        411 : 'Length Required',
+        412 : 'Precondition Failed',
+        413 : 'Request Entity Too Large',
+        414 : 'Request-URI Too Long',
+        415 : 'Unsupported Media Type',
+        416 : 'Requested Range Not Satisfiable',
+        417 : 'Expectation Failed',
+        500 : 'Internal Server Error',
+        501 : 'Not Implemented',
+        502 : 'Bad Gateway',
+        503 : 'Service Unavailable',
+        504 : 'Gateway Timeout',
+        505 : 'HTTP Version Not Supported',
+        
+        #------- Custom Code -----------------
+        529 : 'OverQuotaError',
+        530 : 'Timeout',
+        531 : 'ResponseTooLargeError',
+        532 : 'SSLCertificateError',
+        533 : 'UnAuthorizedError',
+        534 : 'DownloadError',
+        535 : 'GeneralDownloadError',
+    }
+    
+    @classmethod
+    def CodeMap(cls, errCode):
+        des = cls._codeMapDict.get(errCode, None)
+        return '%d %s' % (errCode, des) if des else str(errCode)
+    
+    def __init__(self, host=None, maxfetchcount=2, maxredirect=5, 
+              timeout=CONNECTION_TIMEOUT, addreferer=True, headers=None):
+        self.cookie = Cookie.SimpleCookie()
+        self.maxFetchCount = maxfetchcount
+        self.maxRedirect = maxredirect
+        self.host = host
+        self.addReferer = addreferer
+        self.timeout = timeout
+        self.realurl = ''
+        self.initHeaders = headers
+    
+    def open(self, url, data=None, headers=None):
+        self.realurl = url
+        maxRedirect = self.maxRedirect
+        
+        class resp: #出现异常时response不是合法的对象，使用一个模拟的
+            code=555
+            content=''
+            headers={}
+        
+        response = resp()
+        if url.startswith('data:'):
+            import base64, re
+            rxDataUri = re.compile("^data:(?P<mime>[a-z]+/[a-z]+);base64,(?P<data>.*)$", re.I | re.M | re.DOTALL)
+            m = rxDataUri.match(url)
+            try:
+                response.content = base64.decodestring(m.group("data"))
+                response.code = 200
+            except Exception as e:
+                response.code = 404
+        else:
+            while url and (maxRedirect > 0):
+                cnt = 0
+                while cnt < self.maxFetchCount:
+                    try:
+                        if data and isinstance(data, dict):
+                            data = urllib.urlencode(data)
+                        request = urllib2.Request(url)
+                        h = self._getHeaders(url)
+                        for k in h:
+                            request.add_header(k, h[k])
+                        response = urllib2.urlopen(request)
+                        print response
+                        #break
+                    except Exception as e:
+                        if response.code == 555:
+                            response.code = 535
+                            default_log.warn('url [%s] failed [%s].' % (url, str(e)))
+                        break
+                    else:
+                        break
+                
+                data = None
+                try:
+                    self.SaveCookies(response.header_msg.getheaders('Set-Cookie'))
+                except:
+                    pass
+                
+                if response.code not in [300, 301, 302, 303, 307]: #只处理重定向信息
+                    break
+                
+                urlnew = response.headers.get('Location')
+                if urlnew and not urlnew.startswith("http"):
+                    url = urlparse.urljoin(url, urlnew)
+                else:
+                    url = urlnew
+                maxRedirect -= 1
+        
+        if maxRedirect <= 0:
+            default_log.warn('Too many redirections:%s'%url)
+        
+        self.realurl = url
+        # return response
+        return MyResp(response.code, response.read())
+        
+    def _getHeaders(self, url=None, extheaders=None):
+        headers = {
+             'User-Agent':"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)",
+             'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                  }
+        cookie = '; '.join(["%s=%s" % (v.key, v.value) for v in self.cookie.values()])
+        if cookie:
+            headers['Cookie'] = cookie
+        if self.addReferer and (self.host or url):
+            headers['Referer'] = self.host if self.host else url
+        
+        if self.initHeaders:
+            headers.update(self.initHeaders)
+        if extheaders:
+            headers.update(extheaders)
+        return headers
+        
+    def SaveCookies(self, cookies):
+        if not cookies:
+            return
+        self.cookie.load(cookies[0])
+        for cookie in cookies[1:]:
+            obj = Cookie.SimpleCookie()
+            obj.load(cookie)
+            for v in obj.values():
+                self.cookie[v.key] = v.value
+            
